685a661fd61987c70bf029dec425c96dcc47a516
Fatih Sarıdağ <68101192+fatihsaridag@users.noreply.github.com>
2022-12-20 14:15:07 +0300

    TopicExchange tipi ile RabbitMQ öğrenimi üzerine bir çalışma

---
+++ RabbitmqFirstProject/RabbitmqFirstProject.publisher/Program.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitmqFirstProject.publisher
+{
+    class Program
+    {
+
+
+        public enum LogNames
+        {
+            Critical = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4
+        }
+
+
+        static void Main(string[] args)
+        {
+            //RabbitMq ya bağlanmamız için connection factory isminde bir nesne örneğği alalım
+            var factory = new ConnectionFactory();
+            //Uri yımızı belirticez. CloudAmqp deki instancedan urli alıyoruz. Gerçek seneryoda bunları appsetting.json içeçrisinde tutuyoruz.
+            factory.Uri = new Uri("amqps://eznbdupx:Qf4h0Avxf0yEipy5VaR1D7UHRfIL0Gfn@gerbil.rmq.cloudamqp.com/eznbdupx");
+
+            //factory üzerinden bir bağlantı açıyoruz.
+            using var connection = factory.CreateConnection();
+            //Artık bir bağlantımız var ve bu bağlantı  üzerinden kanal oluşturuyoruz onun üzerinden bağlanıcaz.
+            var channel = connection.CreateModel(); //Bu kanal üzerinden rabbitMq ile haberleşebiliriz.
+            //1.param : Exchange ismi , 2.param : restart atınca uygulama fiziksel olarak kaydedilsin , 3.param : Exchange tipi ? 
+            channel.ExchangeDeclare("logs-topic",durable:true, type:ExchangeType.Topic);                                                        
+           
+ 
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+             {
+
+                 Random rnd = new Random();
+                 LogNames log1 = (LogNames)rnd.Next(1, 5);
+                 LogNames log2 = (LogNames)rnd.Next(1, 5);
+                 LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+                 var routeKey = $"{log1}.{log2}.{log3}";
+                 string message = $"log-type : {log1} - {log2} - {log3}";
+                 var messageBody = Encoding.UTF8.GetBytes(message);
+
+
+
+
+                 channel.BasicPublish("logs-topic", routeKey, null, messageBody);  
+                 Console.WriteLine($"Log Gönderilmiştir : {message}");
+             });
+
+           
+               
+        }
+    }
+}
---
+++ RabbitmqFirstProject/RabbitmqFirstProject.subscriber/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace RabbitmqFirstProject.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            var factory = new ConnectionFactory(); 
+            factory.Uri = new Uri("amqps://eznbdupx:Qf4h0Avxf0yEipy5VaR1D7UHRfIL0Gfn@gerbil.rmq.cloudamqp.com/eznbdupx");  
+            using var connection = factory.CreateConnection(); 
+            var channel = connection.CreateModel();
+
+   
+
+
+            channel.BasicQos(0, 1, false);                                   
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            var routeKey = "*.Error.*";
+            channel.QueueBind(queueName, "logs-topic",routeKey);
+
+
+            channel.BasicConsume(queueName, false,consumer);                  
+            Console.WriteLine("Loglar dinleniyor..");                             
+            consumer.Received += (object sender, BasicDeliverEventArgs e) => {  
+            var message = Encoding.UTF8.GetString(e.Body.ToArray());
+            Thread.Sleep(1500);   
+            Console.WriteLine("Gelen mesaj : " + message);
+            //File.AppendAllText("log-critical.txt", message +"\n");
+            channel.BasicAck(e.DeliveryTag, false); 
+            };
+            Console.ReadLine();
+        }
+    }
+}
